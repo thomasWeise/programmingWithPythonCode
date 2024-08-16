21797cab6dc938cdad23ccbc409183b22a5dcd7a
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-16 16:56:42 +0800

    minor bugfix

--- 01_variables/pi_liu_hui.py
+++ 01_variables/pi_liu_hui.py
@@ -2,7 +2,7 @@ from math import pi, sqrt
 
 print(f"We use Liu Hui's Method to Approximate \u03c0={pi}.")
 e = 6  # the number of edges: We start with a hexagon, i.e., e=6.
-s = 1  # the side length: Initially 1, meaning the radius is also 1.
+s = 1.0  # the side length: Initially 1, meaning the radius is also 1.
 print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
 
 e *= 2  # We double the number of edges...
