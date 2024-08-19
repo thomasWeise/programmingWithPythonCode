24e5169488549fe8dbb9777217aba56d708603d3
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-19 15:01:09 +0800

    minor beautification of mypy script

--- scripts/mypy.sh
+++ scripts/mypy.sh
@@ -5,31 +5,30 @@
 ## $2 is the parameter to mypy
 
 # strict error handling
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o nounset   # set -u : exit the script if you try to use an uninitialized variable
-set -o errexit   # set -e : exit the script if any statement returns a non-true return value
+set -o pipefail  # trace errors through pipes
+set -o errtrace  # trace errors through commands and functions
+set -o nounset   # exit if encountering an uninitialized variable
+set -o errexit   # exit if any statement returns a non-0 return value
 
 # First we check if mypy is installed.
 mypyInfos="$(pip show mypy 2>/dev/null || true)"
 if [ -z "$mypyInfos" ]; then
   # mypy is not installed, so we install it now.
-  # We do this silently, without printing any information to standard out or error...
+  # We do this silently, without printing any information...
   pip install mypy 1>/dev/null 2>&1
   mypyInfos="$(pip show mypy 2>/dev/null)"
 fi
 # Now we can obtain the version of mypy.
-mypyVersion="$(grep Version: <<< "$mypyInfos")"
-mypyVersion="$(sed -n 's/.*Version:\s*\([.0-9]*\)/\1/p' <<< "$mypyVersion")"
+mypyVer="$(grep Version: <<< "$mypyInfos")"
+mypyVer="$(sed -n 's/.*Version:\s*\([.0-9]*\)/\1/p' <<< "$mypyVer")"
 
-# We print the mypy version and we will show the command line which is executed.
+# We print the mypy version and show the command line which is executed.
 echo "\$ mypy $2 --no-strict-optional --check-untyped-defs"
 
 # Now we enter the folder.
 cd "$1"
 
-# Actually execute mypy.
-# Before executing it, we switch of "exit-on-error" and later switch it back on.
+# Switch of "exit-on-error", run mypy, and afterwards switch it back on.
 set +o errexit
 mypy $2 --no-strict-optional --check-untyped-defs --no-color-output 2>&1
 exitCode="$?"
@@ -41,4 +40,5 @@ if (("$exitCode" != 0)) ; then
 else
   exitCodeStr="succeeded"
 fi
-echo "# mypy $mypyVersion $exitCodeStr with exit code $exitCode."
+echo "# mypy $mypyVer $exitCodeStr with exit code $exitCode."
+
