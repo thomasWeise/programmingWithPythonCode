6726dd19b8716a4c2302a63f6b23c93bbd9f3a30
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-22 12:46:06 +0800

    added type hints

--- 01_variables/identity_1.py
+++ 01_variables/identity_1.py
@@ -1,10 +1,10 @@
 # We define sqrt_2 to be a constant with the value of square root of 2.
-sqrt_2 = 1.4142135623730951  # We set the value of the variable.
+sqrt_2: float = 1.4142135623730951  # We set the value of the variable.
 print(f"sqrt_2 = {sqrt_2}")  # We print the value of the variable
 
 # We can also compute the square root using the `sqrt` function.
 from math import sqrt  # Import the root function from the math module.
-sqrt_2_computed = sqrt(2.0)  # Compute the square root of 2.0.
+sqrt_2_computed: float = sqrt(2.0)  # Compute the square root of 2.0.
 print(f"sqrt_2_computed = {sqrt_2_computed}")  # Print the value.
 
 # Let's compare the computed and the constant value:
--- 01_variables/identity_2.py
+++ 01_variables/identity_2.py
@@ -1,20 +1,20 @@
 # String and integer literals and identity.
-a = "Hello World!"
-b = "Hello World!"
+a: str = "Hello World!"
+b: str = "Hello World!"
 print(f"Are 'a' and 'b' the same object: {a is b}")
 
-c = "Hello " + "World!"
+c: str = "Hello " + "World!"
 print(f"Are 'a' and 'c' the same object: {a is c}")
 
-d = "Hello"
+d: str = "Hello"
 d = d + " World!"
 print(f"Are 'a' and 'd' the same object: {a is d}")
 
-e = 10
-mul = 5
-f = (e // mul) * mul
+e: int = 10
+mul: int = 5
+f: int = (e * mul) // mul
 print(f"Are 'e' and 'f' the same object: {e is f}")
 
-g = 1_000_000_000_000_000_000
-h = (g // mul) * mul
+g: int = 1_000_000_000_000_000_000
+h: int = (g * mul) // mul
 print(f"Are 'g' and 'h' the same object: {g is h}")
