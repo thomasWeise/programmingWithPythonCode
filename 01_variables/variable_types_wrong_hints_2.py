7ba8ca43cd004a1887dc28d538457dc8df9f0711
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-22 10:21:40 +0800

    changed example to be more clear

--- 01_variables/variable_types_wrong_hints_2.py
+++ 01_variables/variable_types_wrong_hints_2.py
@@ -1,5 +1,5 @@
-float_var: float = 1 + 7  # Store an int in a variable hinted as float.
-print(type(float_var))  # This prints "<class 'int'>".
+my_var: int | float = 1 + 7  # A variable hinted as either int or float.
+print(type(my_var))  # This prints "<class 'int'>".
 
-float_var = float_var / 3  # / returns a float - as we would except.
-print(type(float_var))  # This now prints "<class 'float'>".
+my_var = my_var / 3  # / returns a float, which is OK.
+print(type(my_var))  # This now prints "<class 'float'>".
