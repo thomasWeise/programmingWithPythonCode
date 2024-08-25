7e3e1341ad3757a88b1686f37c1c17835baf0520
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-25 14:11:24 +0800

    improved output

--- 01_variables/pi_liu_hui.py
+++ 01_variables/pi_liu_hui.py
@@ -1,26 +1,26 @@
 from math import pi, sqrt
 
-print(f"We use Liu Hui's Method to Approximate \u03c0={pi}.")
+print(f"We use Liu Hui's Method to Approximate \u03c0\u2248{pi}.")
 e = 6  # the number of edges: We start with a hexagon, i.e., e=6.
 s = 1.0  # the side length: Initially 1, meaning the radius is also 1.
-print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
+print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
 
 e *= 2  # We double the number of edges...
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
-print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
+print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
 
 e *= 2  # We double the number of edges.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
-print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
+print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
 
 e *= 2  # We double the number of edges.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
-print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
+print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
 
 e *= 2  # We double the number of edges.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
-print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
+print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
 
 e *= 2  # We double the number of edges.
 s = sqrt(2 - sqrt(4 - (s ** 2)))
-print(f"{e} edges, side length={s} give us \u03c0={e * s / 2}.")
+print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
