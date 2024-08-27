ccf5c62fc93a4d39c0b6a0e8a18e027bf8e82e3b
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-27 13:59:13 +0800

    minor fixes and improvements

--- 02_collections/dicts_1.py
+++ 02_collections/dicts_1.py
@@ -2,8 +2,8 @@
 
 num_str: dict[int, str] = {  # create and type hint a dictionary
     1: "one", 2: "two", 3: "three", 4: "four"}  # the elements
-print(f"num_str = {num_str}")  # print the dictionary
-print(f"I got {num_str[2]} shows and {num_str[1]} hat.") # get element
+print(f"num_str has {len(num_str)} elements: {num_str}")  # print dict
+print(f"I got {num_str[2]} shoes and {num_str[1]} hat.") # get element
 
 print(f"the keys are: {list(num_str.keys())}")  # print the keys
 print(f"the values are: {list(num_str.values())}")  # print the values
