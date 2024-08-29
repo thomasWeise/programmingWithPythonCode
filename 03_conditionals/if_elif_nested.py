a26d78665cc6da6eb0d2725a31198c79da987476
Thomas Weise <tweise@hfuu.edu.cn>
2024-08-29 15:20:12 +0800

    fixed indentation

--- 03_conditionals/if_elif_nested.py
+++ 03_conditionals/if_elif_nested.py
@@ -8,7 +8,7 @@ if a > b:  # This means that a > b
     if a > c:  # This means that a > b and a > c.
         print(f"{a} is the greatest number.")
     else:  # This means that a > b and c >= a.
-       print(f"{c} is the greatest number.")
+        print(f"{c} is the greatest number.")
 elif b > c:  # This means that b >= a and b > c.
     print(f"{b} is the greatest number.")
 else:  # This means that b >= a and c >= b.
