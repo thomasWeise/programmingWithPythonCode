a7d6ac6bc7fdb7faf283e60fb6ed68b7e1b4aed2
Thomas Weise <tweise@hfuu.edu.cn>
2024-09-22 18:45:43 +0800

    putting break before continue for didactic reasons

--- 04_loops/for_loop_continue_break.py
+++ 04_loops/for_loop_continue_break.py
@@ -2,10 +2,10 @@
 
 for i in range(15):  # i takes on the values from 0 to 14 one by one.
     s: str = f"i is now {i}."  # Create a string with the value of i.
-    if 5 <= i <= 8:  # If i is in the range of 5..8, then...
-        continue  # ...skip the rest of the loop body, do next iteration.
     if i > 10:  # If i is greater than 10, then...
         break  # ...abort the loop altogether, do not execute next line.
+    if 5 <= i <= 8:  # If i is in the range of 5..8, then...
+        continue  # ...skip the rest of the loop body, do next iteration.
     print(s)  # We get here if neither continue nor break were called.
 
 print("All done.")  # We always get here.
