63f53b12129d59ac5e935a391c9684f53820190e
Thomas Weise <tweise@hfuu.edu.cn>
2024-09-24 05:25:12 +0800

    more efficient prime check

--- 04_loops/for_loop_nested_primes.py
+++ 04_loops/for_loop_nested_primes.py
@@ -1,12 +1,14 @@
 """Compute all primes less than 200 using two nested for loops."""
 
+from math import isqrt  # the integer square root == int(sqrt(...))
+
 primes: list[int] = [2]  # the list for the primes; We know 2 is prime.
 n_divisions: int = 0  # We want to know how many divisions we needed.
 
 for candidate in range(3, 200, 2):  # ...all odd numbers less than 200.
     is_prime: bool = True  # Let us assume that `candidate` is prime.
 
-    for check in range(3, candidate, 2):  # ...odd numbers < candidate
+    for check in range(3, isqrt(candidate) + 1, 2):  # ...odd numbers
         n_divisions += 1  # Every test requires one modulo division.
         if candidate % check == 0:  # modulo == 0: division without rest
             is_prime = False  # If check divides candidate evenly, then
--- 04_loops/for_loop_sequence_primes.py
+++ 04_loops/for_loop_sequence_primes.py
@@ -1,12 +1,17 @@
 """Compute all primes less than 200, with a for loop over a sequence."""
 
+from math import isqrt  # the integer square root == int(sqrt(...))
+
 primes: list[int] = [2]  # the list for the primes; We know 2 is prime.
 n_divisions: int = 0  # We want to know how many divisions we needed.
 
 for candidate in range(3, 200, 2):  # ...all odd numbers less than 200.
     is_prime: bool = True  # Let us assume that `candidate` is prime.
+    limit: int = isqrt(candidate)  # Get the maximum possible divisor.
 
     for check in primes[1:]:  # We only test with the odd primes we got.
+        if check > limit:  # If the potential divisor is too big, then
+            break  # we can stop the inner loop here.
         n_divisions += 1  # Every test requires one modulo division.
         if candidate % check == 0:  # modulo == 0: division without rest
             is_prime = False  # If check divides candidate evenly, then
