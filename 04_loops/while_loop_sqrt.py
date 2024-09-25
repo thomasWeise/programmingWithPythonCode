35382c739e1b4ef1528c4f4fcccfbc723f636098
Thomas Weise <tweise@hfuu.edu.cn>
2024-09-25 12:07:34 +0800

    Implementing Heron's Method using a While Loop

---
+++ 04_loops/while_loop_sqrt.py
@@ -0,0 +1,14 @@
+"""Using a `while` loop to implement the Heron's Method."""
+
+from math import sqrt  # math.sqrt is as exact with float as possible.
+
+for number in [0.5, 2.0, 3.0]:  # The three numbers we want to test.
+    guess: float = 1.0  # This will hold the current guess.
+    old_guess: float = 0.0  # 0.0 is just a dummy value != guess.
+
+    while old_guess != guess:  # Repeat until nothing changes anymore.
+        old_guess = guess  # The current guess becomes the old guess.
+        guess = 0.5 * (guess + number / guess)  # The new guess.
+
+    actual: float = sqrt(number)  # The "actual" computed sqrt value.
+    print(f"\u221A{number}\u2248{guess}, sqrt({number})={actual}")
