ec3c90b1fe245d78615b7c85c9f2adcdc3ca4be7
Thomas Weise <tweise@hfuu.edu.cn>
2024-10-01 07:06:42 +0800

    added Liu Hui's method to approximate pi

--- 05_functions/use_my_math.py
+++ 05_functions/use_my_math.py
@@ -4,4 +4,12 @@ from my_math import factorial, sqrt  # Import our two functions.
 
 print(f"6!={factorial(6)}")  # Use the `factorial` function.
 print(f"\u221A3={sqrt(3.0)}")   # Use the `sqrt` function.
-print(f"\u221A(6! * 1.5)={sqrt(factorial(6) * 1.5)}")  # Use both.
+print(f"\u221A(6!*1.5)={sqrt(factorial(6) * 1.5)}")  # Use both.
+
+print(f"We now use Liu Hui's Method to Approximate \u03c0.")
+e: int = 6  # the number of edges: We start with a hexagon, i.e., e=6.
+s: float = 1.0  # the side length: Initially 1, i.e., radius is also 1.
+for _ in range(6):
+    print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
+    e *= 2  # We double the number of edges...
+    s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
