08bb68a926de250ab895875fcff906ce75efc47e
Thomas Weise <tweise@hfuu.edu.cn>
2024-10-08 12:40:33 +0800

    several improvements

--- 05_functions/normal_pdf_user.py
+++ 05_functions/normal_pdf_user.py
@@ -2,9 +2,20 @@
 
 from normal_pdf import pdf  # import our function
 
-
 print(f"f(0,0,1) = {pdf(0.0)}")  # x is given, default used otherwise.
 print(f"f(2,3,1) = {pdf(2.0, 3.0)}")  # x and mu are given, sigma=1.0.
 print(f"f(-2,7,3) = {pdf(-2.0, 7.0, 3.0)}")  # all three are given.
 print(f"f(-2,0,3) = {pdf(-2.0, sigma=3.0)}")  # x and sigma given.
 print(f"f(0,8,1.5) = {pdf(mu=8.0, x=0.0, sigma=1.5)}")  # unordered...
+
+# We call the function using a dictionary of parameter values.
+args_dict: dict[str, float] = {"x": -2.0, "sigma": 3.0}
+print(f"f(-2,0,3) = {pdf(**args_dict)}")  # notice the double "*" ("**")
+
+# We call the function using a tuple of parameter values.
+args_tup: tuple[float, float, float] = (-2.0, 7.0, 3.0)
+print(f"f(-2,7,3) = {pdf(*args_tup)}")  # notice the single "*"
+
+# We call the function using a list of values, but leave one default.
+args_lst: list[float] = [2.0, 3.0]
+print(f"f(2,3,1) = {pdf(*args_lst)}")  # notice the single "*"
--- 05_functions/use_my_math.py
+++ 05_functions/use_my_math.py
@@ -6,7 +6,7 @@ print(f"6!={factorial(6)}")  # Use the `factorial` function.
 print(f"\u221A3={sqrt(3.0)}")   # Use the `sqrt` function.
 print(f"\u221A(6!*1.5)={sqrt(factorial(6) * 1.5)}")  # Use both.
 
-print(f"We now use Liu Hui's Method to Approximate \u03c0.")
+print("We now use Liu Hui's Method to Approximate \u03c0.")
 e: int = 6  # the number of edges: We start with a hexagon, i.e., e=6.
 s: float = 1.0  # the side length: Initially 1, i.e., radius is also 1.
 for _ in range(6):
