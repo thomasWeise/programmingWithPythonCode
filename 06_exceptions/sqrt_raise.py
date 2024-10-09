8465dfffaf9cee3461e9af4dc2c703db36a13ece
Thomas Weise <tweise@hfuu.edu.cn>
2024-10-09 19:33:40 +0800

    first steps into exceptions

---
+++ 06_exceptions/sqrt_raise.py
@@ -0,0 +1,26 @@
+"""A `sqrt` function raising an error if its result is not finite."""
+
+from math import isfinite
+
+
+def sqrt(number: float) -> float:
+    """
+    Compute the square root of a given `number`.
+
+    :param number: The number to compute the square root of.
+    :return: A value `v` such that `v * v == number`.
+    :raises ArithmeticError: if `number` is not finite or less than `0.0`
+    """
+    if (not isfinite(number)) or (number < 0.0):  # raise error
+        raise ArithmeticError(f"sqrt({number}) is not permitted.")
+    if number <= 0.0:  # Fix for the special case `0`:
+        return 0.0  # We return 0; for now, we ignore negative values.
+    if not isfinite(number):  # Fix for case `+inf` and `nan`:
+        return number  # We return `inf` for `inf` and `nan` for `nan`.
+
+    guess: float = 1.0  # This will hold the current guess.
+    old_guess: float = 0.0  # 0.0 is just a dummy value != guess.
+    while old_guess != guess:  # Repeat until nothing changes anymore.
+        old_guess = guess  # The current guess becomes the old guess.
+        guess = 0.5 * (guess + number / guess)  # The new guess.
+    return guess
---
+++ 06_exceptions/use_sqrt_raise.py
@@ -0,0 +1,9 @@
+"""Using the sqrt function which raises errors."""
+
+from math import inf, nan  # Import infinity and not-a-number from math.
+
+from sqrt_raise import sqrt  # Import our sqrt function.
+
+# Apply our protected square root function to several values.
+for number in (0.0, 1.0, 2.0, 4.0, 10.0, inf, nan, -1.0):
+    print(f"\u221A{number}\u2248{sqrt(number)}")
