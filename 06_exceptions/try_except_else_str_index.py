52b04f0b37615a1e4029b56a2bf7238a7fbbc6a1
Thomas Weise <tweise@hfuu.edu.cn>
2024-10-30 11:47:34 +0800

    a small beautification

--- 06_exceptions/try_except_else_str_index.py
+++ 06_exceptions/try_except_else_str_index.py
@@ -1,10 +1,10 @@
 """Demonstrate `try...except...else` by looking for text in a string."""
 
-text: str = "Hello World!"  # This is the string we search inside.
+r: str = "Hello World!"  # This is the string we search inside.
 
 try:  # If this block raises an error, we continue at `except`.
     for s in ["Hello", "world"]:  # The strings we try to find.
-        print(f"{s!r} is at index {text.index(s)}.")
+        print(f"{s!r} is at index {r.index(s)}.")
 except ValueError as ve:  # ValueError is raised if `s` isn't in `text`.
     print(ve)  # We get here because "world" is not in "Hello World!".
 else:  # The else block is only invoked if no exception occurred.
@@ -12,7 +12,7 @@ else:  # The else block is only invoked if no exception occurred.
 
 try:  # If this block raises an error, we continue at `except`.
     for s in ["Hello", "World"]:  # The strings we will be able to find.
-        print(f"{s!r} is at index {text.index(s)}.")
+        print(f"{s!r} is at index {r.index(s)}.")
 except ValueError as ve:  # ValueError is raised if `s` isn't in `text`.
     print(ve)  # We never get here: This time, we look for "World".
 else:  # The else block is only invoked if no exception occurred.
--- 06_exceptions/try_except_str_index.py
+++ 06_exceptions/try_except_str_index.py
@@ -1,10 +1,10 @@
 """Demonstrate `try...except` by looking for text in a string."""
 
-text: str = "Hello World!"  # This is the string we search inside.
+r: str = "Hello World!"  # This is the string we search inside.
 
 try:  # If this block raises an error, we continue at `except`.
     for s in ["Hello", "world"]:  # The strings we try to find.
-        print(f"{s!r} is at index {text.index(s)}.")
+        print(f"{s!r} is at index {r.index(s)}.")
 except ValueError as ve:  # ValueError is raised if `s` isn't in `text`.
     print(ve)  # We get here because "world" is not in "Hello World!".
 
