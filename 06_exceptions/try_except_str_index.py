e3114ff77b785f1a71e69c6a4725241e72d68aff
Thomas Weise <tweise@hfuu.edu.cn>
2024-10-31 07:58:05 +0800

    slight improvement

--- 06_exceptions/try_except_str_index.py
+++ 06_exceptions/try_except_str_index.py
@@ -3,7 +3,7 @@
 r: str = "Hello World!"  # This is the string we search inside.
 
 try:  # If this block raises an error, we continue at `except`.
-    for s in ["Hello", "world"]:  # The strings we try to find.
+    for s in ["Hello", "world", "!"]:  # The strings we try to find.
         print(f"{s!r} is at index {r.index(s)}.")
 except ValueError as ve:  # ValueError is raised if `s` isn't in `text`.
     print(f"Error: {ve}")  # Error, as "world" is not in "Hello World!".
