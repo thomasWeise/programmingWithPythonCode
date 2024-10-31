c01595ea8c9500ce861744d340a152bc4e586106
Thomas Weise <tweise@hfuu.edu.cn>
2024-11-01 05:50:02 +0800

    an example for try-except-else-finally

---
+++ 06_exceptions/try_except_else_finally.py
@@ -0,0 +1,30 @@
+"""Demonstrate the try-except-else-finally clause."""
+
+
+def divide_and_print(a: int | float, b: int | float) -> None:
+    """
+    Divide the numbers `a` and `b` and print the result.
+
+    :param a: the dividend
+    :param b: the divisor
+    """
+    print(f"\nWe try to divide {a} by {b}.", flush=True)
+
+    try:  # We try to do some computation.
+        print(f"{a} / {b} = {a / b}")  # Try the division and print.
+    except ZeroDivisionError as zd:  # Is b == 0 ?
+        print(f"We got a ZeroDivisionError when doing {a} / {b}: {zd}.")
+    except TypeError as te:  # Has one of the values the wrong type?
+        print(f"We got a TypeError when computing {a} / {b}: {te}.")
+    else:  # Only called when no Exception occurred.
+        print(f"No error occurred when computing {a} / {b}.")
+    finally:  # This code is always called.
+        print(f"We are finally done with division-by-{b}.", flush=True)
+
+    print(f"This code comes after all the {a} by {b} division code.")
+
+
+divide_and_print(10, 5)           # This works just fine.
+divide_and_print(3, 0)            # This yields a ZeroDivisionError.
+divide_and_print("3", 0)          # This yields a TypeError.
+divide_and_print(10 ** 313, 1.0)  # Causes an uncaught OverflowError!
