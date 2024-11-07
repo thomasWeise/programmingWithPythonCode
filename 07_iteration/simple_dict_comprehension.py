4994b2c99c107d743dd7a0ad246b9232985de92d
Thomas Weise <tweise@hfuu.edu.cn>
2024-11-07 18:06:50 +0800

    small change

--- 07_iteration/simple_dict_comprehension.py
+++ 07_iteration/simple_dict_comprehension.py
@@ -1,12 +1,12 @@
 """Simple examples for dictionary comprehension."""
 
 squares_1: dict[int, int] = {}  # We can start with an empty dictionary.
-for i in range(10):  # Then we use a for-loop over the numbers 0 to 9.
+for i in range(11):  # Then we use a for-loop over the numbers 0 to 9.
     squares_1[i] = i * i  # And place the square numbers in the dict.
 print(f" result of construction: {squares_1}")  # Print the result.
 
 # Or we use dictionary comprehension as follows:
-squares_2: dict[int, int] = {i: i ** 2 for i in range(10)}
+squares_2: dict[int, int] = {i: i ** 2 for i in range(11)}
 print(f"result of comprehension: {squares_2}")  # Print the result.
 
 # Compute the largest divisors of numbers which are not prime.
