1b9264ff3abd2628a3dd110fd742c524df8ea9f3
Thomas Weise <tweise@hfuu.edu.cn>
2024-11-07 12:18:50 +0800

    added doctests

--- 07_iteration/list_flatten_iterables.py
+++ 07_iteration/list_flatten_iterables.py
@@ -5,12 +5,15 @@ from typing import Iterable
 
 def flatten(iterables: Iterable[Iterable]) -> list:
     """
-    Flatten an :class:`Iterable` of `Iterable`s.
+    Flatten an :class:`Iterable` of `Iterable`s to a flat list.
 
     :param iterables: the `Iterable` containing other `Iterable`s.
-    :return: a single list with all the contents
+    :return: a list with all the contents of the nested `Iterable`s.
 
     >>> flatten([[1, 2, 3], [4, 5, 6]])
+    [1, 2, 3, 4, 5, 6]
+
+    >>> flatten([[1, 2, 3], [], [4, 5, 6]])
     [1, 2, 3, 4, 5, 6]
 
     >>> flatten(([1, 2, 3], (4, 5, 6), {"a": 7, "b": 8}))
---
+++ scripts/pytest_doctest.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -
+
+# This is our internal pytest-doctest execution script.
+# It prints the pytest-doctest command and the results of pytest.
+# It always exits with an exit code 0, even if pytest fails.
+# Argument $1: the folder to run it in
+# Argument $2: the file(s) to scan
+
+# We enforce strict error handling, i.e., fail on any unexpected error.
+set -o pipefail  # trace errors through pipes
+set -o errtrace  # trace errors through commands and functions
+set -o nounset   # exit if encountering an uninitialized variable
+set -o errexit   # exit if any statement returns a non-0 return value
+
+# Check if pytest and all required plugins are installed.
+versions=""  # This variable will receive all tool versions.
+for pack in "pytest" "pytest-timeout"; do
+  infos="$(python3 -m pip show "$pack" 2>/dev/null || true)"
+  if [ -z "$infos" ]; then
+    # pytest or the plugin is not installed, so we install it now.
+    # We do this silently, without printing any information...
+    python3 -m pip install "$pack" 1>/dev/null 2>&1
+    infos="$(python3 -m pip show "$pack" 2>/dev/null)"
+  fi
+
+  # For each tool or plugin, we get the version separately.
+  infos="$(grep Version: <<< "$infos")"
+  infos="$(sed -n 's/.*Version:\s*\([.0-9]*\)/\1/p' <<< "$infos")"
+  if [ -z "$versions" ]; then  # ... and we concatenate them
+    versions="$pack $infos with"
+  elif [[ "$versions" == *with ]]; then
+    versions="$versions $pack $infos"
+  else
+    versions="$versions, $pack $infos"
+  fi
+done
+
+# Construct the pytest command for documentation testing.
+command="pytest --timeout=10 --no-header --tb=short --doctest-modules $2"
+echo "\$ $command"  # We print the command line which will be executed.
+cd "$1"  # We enter the folder inside of which we should execute pytest.
+export COLUMNS=73
+# Switch of "exit-on-error", run pytest, and afterwards switch it back on.
+set +o errexit  # Turn off exit-on-error.
+$command 2>&1
+exitCode="$?"  # Store exit code of program in variable exitCode.
+set -o errexit  # Turn exit-on-error back on.
+
+# Convert exit code to success or failure string.
+[ "$exitCode" -eq 0 ] && exitCodeStr="succeeded" || exitCodeStr="failed"
+
+# Finally, we print the result string.
+echo "# $versions $exitCodeStr with exit code $exitCode."
