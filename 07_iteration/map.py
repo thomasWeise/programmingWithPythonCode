c4715e9abf744e0ce90f5ff5ee983bdb64fb3a20
Thomas Weise <tweise@hfuu.edu.cn>
2024-11-14 14:11:06 +0800

    better documentation

--- 07_iteration/map.py
+++ 07_iteration/map.py
@@ -1,12 +1,16 @@
-"""Examples of `map`."""
+"""Examples of `map`: Transform the elements of sequences."""
 
+# A string with comma-separated values (csv).
 csv_data: str = "12,23,445,3,12,6,-3,5"
 
+# Convert the textual data to a list of int by using `split` and `map`.
 csv_data_as_ints: list[int] = list(map(int, csv_data.split(",")))
 print(f"csv_data_as_ints: {csv_data_as_ints}")
 
+# Obtain all unique square numbers using `map` and `set`.
 csv_data_sqrs: set[int] = set(map(lambda x: x ** 2, csv_data_as_ints))
 print(f"csv_data_sqrs: {csv_data_sqrs}")
 
+# Get the maximum word length by using `map`, `len`, and `max`.
 words: list[str] = ["Hello", "world", "How", "are", "you"]
 print(f"longest word length: {max(map(len, words))}")
