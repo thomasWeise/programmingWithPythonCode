10024d5feeac7dde9a584db526b7faaa52b0eeb5
Thomas Weise <tweise@hfuu.edu.cn>
2024-11-14 14:23:31 +0800

    again slightly modified example

--- 07_iteration/map.py
+++ 07_iteration/map.py
@@ -1,16 +1,16 @@
 """Examples of `map`: Transform the elements of sequences."""
 
 # A string with comma-separated values (csv).
-csv_data: str = "12,23,445,3,12,6,-3,5"
+csv_text: str = "12,23,445,3,12,6,-3,5"
 
 # Convert the csv data to ints by using `split` and `map`, then filter.
-for k in filter(lambda x: x > 20, map(int, csv_data.split(","))):
+for k in filter(lambda x: x > 20, map(int, csv_text.split(","))):
     print(f"found value {k}.")
 
 # Obtain all unique square numbers using `map` and `set`.
-csv_data_sqrs: set[int] = set(map(
-    lambda x: int(x) ** 2, csv_data.split(",")))
-print(f"csv_data_sqrs: {csv_data_sqrs}")
+csv_text_sqrs: set[int] = set(map(
+    lambda x: int(x) ** 2, csv_text.split(",")))
+print(f"csv_text_sqrs: {csv_text_sqrs}")
 
 # Get the maximum word length by using `map`, `len`, and `max`.
 words: list[str] = ["Hello", "world", "How", "are", "you"]
