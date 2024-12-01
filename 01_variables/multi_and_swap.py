2861b70496ef07bf01b60a8bea78ae023e3e148f
Thomas Weise <tweise@hfuu.edu.cn>
2024-12-01 16:27:26 +0800

    using { = } f-strings more often

--- 01_variables/multi_and_swap.py
+++ 01_variables/multi_and_swap.py
@@ -1,11 +1,11 @@
 a, b = 5, 10
-print(f"a={a}, b={b}")
+print(f"{a = }, {b = }")
 
 a, b = b, a
-print(f"a={a}, b={b}")
+print(f"{a = }, {b = }")
 
 z, y, x = 1, 2, 3
-print(f"x={x}, y={y}, z={z}")
+print(f"{x = }, {y = }, {z = }")
 
 x, y, z = z, y, x
-print(f"x={x}, y={y}, z={z}")
+print(f"{x = }, {y = }, {z = }")
