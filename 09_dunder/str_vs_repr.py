bc9f126416e86c38002a13763748601d39e0a453
Thomas Weise <tweise@hfuu.edu.cn>
2024-12-08 09:26:51 +0800

    even better examples for str and repr

--- 09_dunder/str_vs_repr.py
+++ 09_dunder/str_vs_repr.py
@@ -2,6 +2,18 @@
 
 from datetime import UTC, datetime
 
+the_int: int = 123  # An integer with value 123.
+print(the_int)  # This is identical to `print(str(the_int))`
+print(repr(the_int))  # Prints the same as above.
+
+the_string: str = "123"  # A string, with value "123".
+print(the_string)  # This is identical to `print(str(the_string))`.
+print(repr(the_string))  # Notice the added `'` around the string.
+
+l1: list[int] = [1, 2, 3]  # A list of integers.
+l2: list[str] = ['1', '2', '3']  # A list of strings.
+print(f"{l1 = }, but {l2 = }")  # str(list) uses repr for list elements.
+
 # Get the date and time when this program was run.
 right_now: datetime = datetime.now(tz=UTC)
 
