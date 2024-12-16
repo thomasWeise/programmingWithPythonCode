99345519e54aaeb06bbf6050bb963d7e1e4fce86
Thomas Weise <tweise@hfuu.edu.cn>
2024-12-16 12:46:59 +0800

    improved comments for documentation

--- 06_exceptions/file_try_finally.py
+++ 06_exceptions/file_try_finally.py
@@ -6,15 +6,15 @@ from typing import IO  # IO is the text stream object
 # We open the text file "example.txt" for writing.
 stream_out: IO = open("example.txt", mode="w", encoding="UTF-8")
 try:  # If we succeed opening the file for writing, then...
-    stream_out.write("Hello world!")  # Write one line of text.
-finally:                # ...we will definitely get here....
+    stream_out.write("Hello world!")  # ...we write one line of text
+finally:                # ...and we will definitely get here....
     stream_out.close()  # and close the file again.
 
 # We now open the text file "example.txt" for reading.
 stream_in: IO = open("example.txt", encoding="UTF-8")
 try:  # If we succeed opening the file for reading, then...
-    print(stream_in.readline())  # Read one line of text.
-finally:               # ...we will definitely get here....
+    print(stream_in.readline())  # ...we read one line of text
+finally:               # ...and we will definitely get here....
     stream_in.close()  # and close the file again.
 
 remove("example.txt")  # Delete the file "example.txt".
