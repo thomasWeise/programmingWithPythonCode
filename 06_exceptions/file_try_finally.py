4b1c2d32f1e6d1fd03fe51e3997669999be0dc27
Thomas Weise <tweise@hfuu.edu.cn>
2024-12-16 12:06:31 +0800

    slight improvement

--- 06_exceptions/file_try_finally.py
+++ 06_exceptions/file_try_finally.py
@@ -1,17 +1,17 @@
 """Use the `try-finally` statement to write to and read from a file."""
 
 from os import remove  # Needed to delete a file.
-from typing import IO, Final  # IO is the text stream object
+from typing import IO  # IO is the text stream object
 
 # We open the text file "example.txt" for writing.
-stream_out: Final[IO] = open("example.txt", mode="w", encoding="UTF-8")
+stream_out: IO = open("example.txt", mode="w", encoding="UTF-8")
 try:  # If we succeed opening the file for writing, then...
     stream_out.write("Hello world!")  # Write one line of text.
 finally:                # ...we will definitely get here....
     stream_out.close()  # and close the file again.
 
 # We now open the text file "example.txt" for reading.
-stream_in: Final[IO] = open("example.txt", encoding="UTF-8")
+stream_in: IO = open("example.txt", encoding="UTF-8")
 try:  # If we succeed opening the file for reading, then...
     print(stream_in.readline())  # Read one line of text.
 finally:               # ...we will definitely get here....
