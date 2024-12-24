8b5965ba191764c2b33d4607244ba153140f7ebe
Thomas Weise <tweise@hfuu.edu.cn>
2024-12-24 14:15:01 +0800

    slightly improved script

--- 10_packages/numpy_user_venv.sh
+++ 10_packages/numpy_user_venv.sh
@@ -14,14 +14,16 @@ echo "# We install the package 'numpy' into the virtual environment."
 pip install numpy
 
 echo "# 'numpy' is now available for Python programs."
-python3 numpy_user.py
+echo "$ python3 numpy_user.py"
+python3 numpy_user.py 2>&1
 
 echo "# We deactivate the virtual environment."
 echo "# This means that programs now use the system environment only."
 deactivate
 
 echo "# 'numpy' is no longer available (unless installed system-wide)."
-python3 numpy_user.py || true
+echo "$ python3 numpy_user.py"
+python3 numpy_user.py 2>&1 || true
 
 echo "# We could re-use the virtual environment by activating it again."
 echo "# However, we delete the directory to clean up after the example."
