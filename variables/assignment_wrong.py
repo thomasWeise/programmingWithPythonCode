5c2f190d94064046dda61b261642b61c1bf149cd
Thomas Weise <tweise@hfuu.edu.cn>
2025-01-27 18:50:34 +0800

    added error assignment program

---
+++ variables/assignment_wrong.py
@@ -0,0 +1,18 @@
+# We define a variable named "int_var" and assign the int value 1 to it.
+int_var = 1
+
+# We can use the variable int_var in computations like any other value.
+print(2 + int_var)  # This should print 2 + int_var = 2 + 1 = 3.
+
+# We can also use the variable in f-strings.
+print(f"int_var has value {int_var}.")  # prints 'int_var has value 1.'
+
+# We can also change the value of the variable.
+int_var = (3 * int_var) + 1  # int_var = (3 * 1) + 1 = 4
+print(f"int_var is now {intvar}.")  # prints 'int_var is now 4.'
+
+float_var = 3.5  # Ofcourse we can also use floating point numbers.
+print(f"float_var has value {float_var}.")  # 'float_var has value 3.5.'
+
+new_var = float_var * int_var  # new_var = 3.5 * 4 = 14.0 <- a float!
+print(f"{new_var = }.")
