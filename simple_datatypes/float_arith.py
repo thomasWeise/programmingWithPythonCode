39d4a43e4c1d6f63182f4de84dc903df90b9542f
Thomas Weise <tweise@hfuu.edu.cn>
2025-07-12 18:50:28 +0800

    float arithmetic example 1

---
+++ simple_datatypes/float_arith.py
@@ -0,0 +1,21 @@
+6 / 3
+1.0 + 7
+5 - 3.6
+2 * 3.0
+6.5 % 2
+3.3 ** 0.5
+((3.4 * 5.5) - 1.2) ** (4.4 / 3.3)
+from math import pi, e
+pi
+e
+0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1
+0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 - 1.0
+from math import sin, cos, tan, log
+sin(0.25 * pi) ** 2
+cos(pi / 3)
+tan(pi / 4)
+log(e ** 10)
+from math import asin, acos, atan
+asin(sin(0.925))
+acos(cos(-0.3))
+atan(tan(1))
