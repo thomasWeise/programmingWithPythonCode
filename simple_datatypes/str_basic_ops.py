ae489e7cbe8a463d3fc78c7a33352ba04dbb3337
Thomas Weise <tweise@hfuu.edu.cn>
2025-07-15 10:00:40 +0800

    more repl examples

---
+++ simple_datatypes/str_basic_ops.py
@@ -0,0 +1,30 @@
+"World" in "Hello World!"
+"Earth" in "Hello World!"
+
+"Hello World!".find("World")
+"Hello World!".find("world")
+"Hello World!".find("l")
+"Hello World!".find("l", 3)
+"Hello World!".find("l", 4)
+"Hello World!".find("l", 10)
+
+"Hello World!".rfind("l")
+"Hello World!".rfind("l", 0, 9)
+"Hello World!".rfind("l", 0, 3)
+"Hello World!".rfind("l", 0, 2)
+
+"Hello World!".replace("Hello", "Hi")
+"Hello Hello World!".replace("Hello", "Hi")
+
+" Hello World! ".strip()
+" Hello World! ".lstrip()
+" Hello World! ".rstrip()
+
+"Hello World!".lower()
+"Hello World!".upper()
+
+"Hello World!".startswith("hello")
+"Hello World!".startswith("Hello")
+
+"Hello World!".endswith("Hello")
+"Hello World!".endswith("World!")
---
+++ simple_datatypes/str_escape.py
@@ -0,0 +1,12 @@
+print("\"")
+print("\'")
+print("\\")
+print("\"\'\\")
+
+print("Hello\nWorld!")
+print("Hello\r\nWorld!")
+
+print("The horizontal tab is like a bigger space: '\t'.")
+
+>>> print("Hello\
+... World!")
