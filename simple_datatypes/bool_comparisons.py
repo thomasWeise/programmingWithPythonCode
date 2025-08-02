4924ee5038abc4407172a414024a4c9a5c696885
Thomas Weise <tweise@hfuu.edu.cn>
2025-08-02 13:31:56 +0800

    added comments

--- simple_datatypes/bool_comparisons.py
+++ simple_datatypes/bool_comparisons.py
@@ -1,18 +1,18 @@
-
+# Compare a number to itself.
 6 >  6
 6 >= 6
 6 == 6
 6 <= 6
 6 <  6
 6 != 6
-
+# Compare a smaller to a larger number.
 5 >  6
 5 >= 6
 5 == 6
 5 <= 6
 5 <  6
 5 != 6
-
+# Compare a larger to a smaller number.
 6 >  5
 6 >= 5
 6 == 5
@@ -20,11 +20,11 @@
 6 <  5
 6 != 5
 
-5.5 == 5
-5.0 == 5
+5.5 == 5  # compare a float value with an int: False due to ".5"
+5.0 == 5  # compare the float 5.0 with the int 5: True!
 
-3 <  4 < 5 <  6
-5 >= 4 > 4 >= 3
+3 <  4 < 5 <  6  # Chained comparison: True, all comparisons hold.
+5 >= 4 > 4 >= 3  # Chained comparison: False, as 4 is not ">" 4.
 
-type(True)
-type(5 == 5)
+type(True)     # type(x) returns the datatype of x.
+type(5 == 5)   # type(5 == 5) is the same as type(True), i.e., bool.
--- simple_datatypes/bool_logic.py
+++ simple_datatypes/bool_logic.py
@@ -1,16 +1,21 @@
+# Conjunction (and) is True iff both operands are True.
 False and False
 False and True
 True  and False
 True  and True
 
+# Disjunction (or) is True iff at least one operand is True.
 False or False
 False or True
 True  or False
 True  or True
 
+# Negation (not) is True iff its operand is False.
 not True
 not False
 
+# Operators can be combined and grouped with parentheses.
 (True or False) and ((False or True) or (False and False))
 
+# Operators can accept any expression of result type bool.
 (5 < 4) or (6 < 9 < 8)
