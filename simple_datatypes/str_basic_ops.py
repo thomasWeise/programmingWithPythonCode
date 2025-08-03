c1e02b3f9385c46952b4d6b80e789ee367540758
Thomas Weise <tweise@hfuu.edu.cn>
2025-08-03 09:50:45 +0800

    added some comments and made minor improvements

--- simple_datatypes/str_basic_ops.py
+++ simple_datatypes/str_basic_ops.py
@@ -1,30 +1,31 @@
-"World" in "Hello World!"
-"Earth" in "Hello World!"
+"World" in "Hello World!"  # check if string is contained: yes
+"Earth" in "Hello World!"  # check if string is contained: no
 
-"Hello World!".find("World")
-"Hello World!".find("world")
-"Hello World!".find("l")
-"Hello World!".find("l", 3)
-"Hello World!".find("l", 4)
-"Hello World!".find("l", 10)
+"Hello World!".find("World")  # get index of substring
+"Hello World!".find("world")  # result -1 means "not found"
+"Hello World!".find("l")      # search from start of string
+"Hello World!".find("l", 3)   # search starting at index 3
+"Hello World!".find("l", 4)   # search starting at index 4
+"Hello World!".find("l", 10)  # starting at index 10: not found
 
-"Hello World!".rfind("l")
-"Hello World!".rfind("l", 0, 9)
-"Hello World!".rfind("l", 0, 3)
-"Hello World!".rfind("l", 0, 2)
+"Hello World!".rfind("l")        # search backwards from end
+"Hello World!".rfind("l", 2, 9)  # search backwards in range [2, 9)
+"Hello World!".rfind("l", 0, 3)  # search backwards in range [0, 3)
+"Hello World!".rfind("l", 0, 2)  # search backwards in range [0, 2)
 
-"Hello World!".replace("Hello", "Hi")
-"Hello Hello World!".replace("Hello", "Hi")
+"Hello World!".replace("Hello", "Hi")             # replace all
+"Hello World! Hello!".replace("Hello", "Hi")      # occurrences, but
+"Hello World!".replace("Hello", "Hello! Hello!")  # not recursively
 
-" Hello World! ".strip()
-" Hello World! ".lstrip()
-" Hello World! ".rstrip()
+" Hello World! ".strip()   # Remove leading and trailing spaces.
+" Hello World! ".lstrip()  # Remove leading spaces.
+" Hello World! ".rstrip()  # Remove trailing spaces.
 
-"Hello World!".lower()
-"Hello World!".upper()
+"Hello World!".lower()  # Convert to lower case.
+"Hello World!".upper()  # Convert to upper case.
 
-"Hello World!".startswith("hello")
-"Hello World!".startswith("Hello")
+"Hello World!".startswith("hello")  # Checking if string starts with
+"Hello World!".startswith("Hello")  # text is case-sensitive.
 
-"Hello World!".endswith("Hello")
-"Hello World!".endswith("World!")
+"Hello World!".endswith("Hello")   # Checking if string ends with
+"Hello World!".endswith("World!")  # text is case-sensitive, too.
