dc0d7f6fc5e56582f1d47c66faa9e92cb6a09740
Thomas Weise <tweise@hfuu.edu.cn>
2025-08-05 10:08:58 +0800

    added some more comments

--- simple_datatypes/str_escape.py
+++ simple_datatypes/str_escape.py
@@ -1,12 +1,19 @@
-print("\"Hello World!\"")
-print("\'Hello World\'")
-print("\\")
-print("\"\'\\\"\"")
+print("A single quotation mark: '")   # We can simply use the
+print('A double quotation mark: "')   # "other" quotation mark...
 
-print("Hello\nWorld!")
-print("Hello\r\nWorld!")
+# But now we need to use escaping.
+print("A single (') and a double (\") quotation mark.")
+
+print("\"Hello World!\"")  # We simply put a backslash (\) before
+print("\'Hello World\'")   # the forbidden character.
+print("\\")                # Backslashes are escaped like this, too.
+print("\"\'\\\"\"")        # This becomes "'\""
+
+print("Hello\nWorld!")     # \n is a newline.
+print("Hello\r\nWorld!")   # On Windows systems, \r\n is common.
 
 print("The horizontal tab is like a bigger space: '\t'.")
 
+# A backslash before a line break removes the line break.
 >>> print("Hello \
 ... World!")
--- simple_datatypes/str_from.py
+++ simple_datatypes/str_from.py
@@ -1,11 +1,11 @@
-int("1111")
-int("0x1111", 16)
-int("0b1111", 2)
+int("1111")        # Convert decimal number 1111 to int.
+int("0x1111", 16)  # Convert hexadecimal number 1111 to int.
+int("0b1111", 2)   # Convert binary number 1111 to int.
 
-float("2.233e4")
-float("0.1123")
-float("inf")
-float("nan")
+float("2.233e4")   # fractional number in scientific notation and
+float("0.1123")    # normal fractional number converted to float
+float("inf")       # convert to float value inf
+float("nan")       # convert to float value not-a-number
 
-bool("True")
-bool("False")
+bool("True")       # convert to the bool value True
+bool("False")      # convert to the bool value False
