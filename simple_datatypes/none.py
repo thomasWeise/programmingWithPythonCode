83346b878e81ebd57c00408579c1028dc90c0c66
Thomas Weise <tweise@hfuu.edu.cn>
2025-08-07 18:49:34 +0800

    better comments

--- simple_datatypes/none.py
+++ simple_datatypes/none.py
@@ -1,15 +1,18 @@
 None                          # Prints nothing.
 print(None)                   # Prints "None".
-1 is None                     # False
-"Hello World!" is None        # also False
 
-# Different from 'nan == nan', which is False, 'None == None' is
-# True. However, using '==' is discouraged if 'None' is involved.
-None == None
+# The use the equality comparisons via == is discouraged if None may
+# be involved. We do it here anyway, just to see what it does.
+1 == None                     # False
+"Hello World!" == None        # also False
+# Different from 'nan == nan', which is False, 'None == None' is...
+None == None                  # ...True
 
 # It is encouraged to use 'is' instead, which checks object identity
 # (not equality like ==).
-None is None
+1 is None                     # False
+"Hello World!" is None        # False
+None is None                  # True
 
 print(print("Hello World!"))  # Prints "Hello World!" and "None".
 type(None)                    # None is the only instance of NoneType
