eb453824d24496001927c08752769038faaf0df6
Thomas Weise <tweise@hfuu.edu.cn>
2025-08-29 07:26:29 +0800

    changed lx to lstx to avoid l looking like 1 misunderstandings

--- collections/lists_3.py
+++ collections/lists_3.py
@@ -1,26 +1,26 @@
 """An example of more operations with lists."""
 
-l1: list[int] = [1, 2, 3, 4]  # create first list
-l2: list[int] = [5, 6, 7]  # create second list
-l3: list[int] = l1 + l2  # l3 = concatenation of l1 and l2.
-print(f"l3 = l1 + l2 == {l3}")  # [1, 2, 3, 4, 5, 6, 7]
+lst1: list[int] = [1, 2, 3, 4]  # create first list
+lst2: list[int] = [5, 6, 7]  # create second list
+lst3: list[int] = lst1 + lst2  # lst3 = concatenation of lst1 and lst2.
+print(f"lst3 = lst1 + lst2 == {lst3}")  # [1, 2, 3, 4, 5, 6, 7]
 
-l4: list[int] = l2 * 3  # l4 = l2, repeated three times.
-print(f"l4 = l2 * 3 == {l4}")  # [5, 6, 7, 5, 6, 7, 5, 6, 7]
+lst4: list[int] = lst2 * 3  # lst4 = lst2, repeated three times.
+print(f"lst4 = lst2 * 3 == {lst4}")  # [5, 6, 7, 5, 6, 7, 5, 6, 7]
 
-l5: list[int] = l4[2:-2]  # l5 = l4 from index 2 to 3rd from end
-print(f"l5 = l4[2:-2] == {l5}")  # [7, 5, 6, 7, 5]
+lst5: list[int] = lst4[2:-2]  # lst5 = lst4 from index 2 to 3rd from end
+print(f"lst5 = lst4[2:-2] == {lst5}")  # [7, 5, 6, 7, 5]
 
-l6: list[int] = l4[1::2]  # start at index 1, take every 2nd element
-print(f"l6 = l4[1::2] == {l6}")  # [6, 5, 7, 6]
+lst6: list[int] = lst4[1::2]  # start at index 1, take every 2nd element
+print(f"lst6 = lst4[1::2] == {lst6}")  # [6, 5, 7, 6]
 
-# Start copying l4 at last element, move backwards take every 2nd
+# Start copying lst4 at last element, move backwards take every 2nd
 # element, and stop right before index=3.
-l7: list[int] = l4[-1:3:-2]
-print(f"l7 = l4[-1:3:-2] == {l7}")  # [7, 5, 6]
+lst7: list[int] = lst4[-1:3:-2]
+print(f"lst7 = lst4[-1:3:-2] == {lst7}")  # [7, 5, 6]
 
-l7[1] = 12  # Modify the slice l7 originally from l4.
-print(f"{l4 = }, {l7 = }")  # Shows that l4 remains unchanged.
+lst7[1] = 12  # Modify the slice lst7 originally from lst4.
+print(f"{lst4 = }, {lst7 = }")  # Shows that lst4 remains unchanged.
 
-a, b, c = l2  # store the three elements of l2 into variables
+a, b, c = lst2  # store the three elements of lst2 into variables
 print(f"{a = }, {b = }, {c = }")  # a=5, b=6, c=7
