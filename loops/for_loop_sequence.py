2e551b300fc92bcbbaf97d414a2617097d426e42
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-01 14:14:20 +0800

    minor improvements

--- loops/for_loop_sequence.py
+++ loops/for_loop_sequence.py
@@ -4,15 +4,15 @@ txt: list[str] = []  # We will collect the output text in this list.
 
 lst: list[int] = [1, 2, 3, 50]  # Create a list with 4 integers.
 for i in lst:  # i takes on the values 1, 2, 3, and 50.
-    txt.append(f"i={i}")  # We store "i=1", "i=2", "i=3", and "i=50"
+    txt.append(f"{i = }")  # We store "i = 1", "i = 2", "i = 3"...
 
 tp: tuple[float, ...] = (7.6, 9.4, 8.1)  # Create a tuple with 3 floats.
 for f in tp:  # i takes on the values 7.6, 9.4, and 8.1.
-    txt.append(f"f={f}")  # We store "f=7.6", "f=9.4", and "f=8.1"
+    txt.append(f"{f = }")  # We store "f = 7.6", "f = 9.4", ...
 
 st: set[str] = {"u", "v", "w"}  # Create a set with 3 strings.
 for s in st:  # s takes on the values "u", "v", and "w" (unordered!).
-    txt.append(f"s={s!r}")  # We store "s='u'", "s='v'", and "s='w'".
+    txt.append(f"s = {s!r}")  # We store "s = 'u'", "s = 'v'", ...
 
 dc: dict[float, bool] = {1.1: True, 2.5: False}  # Create a dictionary.
 for k in dc:  # Iterate over the keys in the dictionary == 1.1 and 2.5.
