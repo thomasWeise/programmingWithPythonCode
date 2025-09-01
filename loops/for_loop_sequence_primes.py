d6a127a81be65ef8d9813819b68ef0b1098ff3f6
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-01 15:02:01 +0800

    simplified program

--- loops/for_loop_sequence_primes.py
+++ loops/for_loop_sequence_primes.py
@@ -2,14 +2,14 @@
 
 from math import isqrt  # the integer square root == int(sqrt(...))
 
-primes: list[int] = [2]  # the list for the primes; We know 2 is prime.
+primes: list[int] = []  # The list for the primes is initially empty.
 n_divisions: int = 0  # We want to know how many divisions we needed.
 
 for candidate in range(3, 200, 2):  # ...all odd numbers less than 200.
     is_prime: bool = True  # Let us assume that `candidate` is prime.
     limit: int = isqrt(candidate)  # Get the maximum possible divisor.
 
-    for check in primes[1:]:  # We only test with the odd primes we got.
+    for check in primes:  # We only test with the odd primes we got.
         if check > limit:  # If the potential divisor is too big, then
             break          # we can stop the inner loop here.
         n_divisions += 1  # Every test requires one modulo division.
@@ -19,6 +19,8 @@ for candidate in range(3, 200, 2):  # ...all odd numbers less than 200.
 
     if is_prime:  # If True: no smaller number divides candidate evenly.
         primes.append(candidate)  # Store candidate in primes list.
+
+primes.insert(0, 2)  # Now we insert 2 at the beginning of the list.
 
 # Finally, print the list of prime numbers.
 print(f"After {n_divisions} divisions: {len(primes)} primes {primes}.")
