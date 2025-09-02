b67f1a7f3d8caa4008a34bc72bf0196062d259a8
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-02 13:25:05 +0800

    improved documentation

--- loops/for_loop_enumerate.py
+++ loops/for_loop_enumerate.py
@@ -1,5 +1,12 @@
-"""Enumerate the index-value pairs in a list using enumerate."""
+"""
+Enumerate the index-value pairs in a list using enumerate.
 
-data: list[int] = [1, 2, 3, 5]
-for i, v in enumerate(data):
+We now use the function `enumerate` to generate a sequence of
+index-value pairs and directly unpack them in the loop header into the
+variables `i` (for the index) and `v` (for the value).
+"""
+
+data: list[int] = [1, 2, 3, 5]  # The data to iterate over.
+
+for i, v in enumerate(data):  # Generate index-value pair sequence...
     print(f"data[{i}]={v}")
