5545f9081c90f18eecbda3b59880aeea2d33e6a7
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-11 07:54:45 +0800

    slight change

--- exceptions/try_multi_except.py
+++ exceptions/try_multi_except.py
@@ -7,10 +7,10 @@ sqrt_of_1_div_0: float  # Declare this variable, but do not assign it.
 try:  # If this block raises an error, we continue at `except`.
     # sqrt_of_1_div_0 only gets assigned if sqrt(1 / 0) succeeds...
     sqrt_of_1_div_0 = sqrt(1 / 0)  # Which error will this produce?
-except ZeroDivisionError as de:  # A division by zero?
-    print(f"We got a division-by-zero error: {de}.", flush=True)
+except ZeroDivisionError:  # Did a division by zero happen?
+    print(f"We got a division-by-zero error!", flush=True)
 except ArithmeticError as ae:  # Or an ArithmeticError?
-    print(f"We got an arithmetic error: {ae}.", flush=True)
+    print(f"We got an arithmetic error: {ae}!", flush=True)
 
 print("Now we try to print the value of sqrt_of_1_div_0.", flush=True)
 print(sqrt_of_1_div_0)  # Does not work: sqrt_of_1_div_0 is not assigned
