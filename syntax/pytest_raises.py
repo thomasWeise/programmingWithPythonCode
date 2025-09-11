f3eb77ca4ca52ba7ad14423fdc78270ed98617f4
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-12 07:49:17 +0800

    slight improvement

--- syntax/pytest_raises.py
+++ syntax/pytest_raises.py
@@ -1,15 +1,15 @@
-"""The syntax of the `raises` ContextManager offered by `pytest`."""
+"""The syntax of the `raises` context manager offered by `pytest`."""
 
 from pytest import raises  # Needed checking that exceptions are raised.
 
-# `raises` is a ContextManager that will cause the test to fail if no
+# `raises` is a context manager that will cause the test to fail if no
 # exception of type `ExceptionType` is raised.
 with raises(ExceptionType):
     code that should raise ExceptionType
 
 # We can provide a regular expression with parameter `match`.
 # If either no exception of type `ExceptionType` is raised
-# >>> OR <<<<
+#                   >>> OR <<<<
 # If the error message stored in the exception does not match to this
 # regex, then the test will fail.
 with raises(ExceptionType, match="error message regex"):
