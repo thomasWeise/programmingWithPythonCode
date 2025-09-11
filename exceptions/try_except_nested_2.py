6c716bd6f08314bd9acf6966d0bc3041bf615c33
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-11 08:32:00 +0800

    improved example

--- exceptions/try_except_nested_2.py
+++ exceptions/try_except_nested_2.py
@@ -1,8 +1,9 @@
 """Demonstrate nested `try...except` blocks."""
 
+from math import nan
 from sqrt_raise import sqrt  # Import our sqrt function.
 
-sqrt_of_1_div_0: float  # Declare this variable, but do not assign it.
+sqrt_of_1_div_0: float = nan  # Declare this variable and assign it.
 
 try:  # If this block raises an error, we continue at `except`.
     # sqrt_of_1_div_0 only gets assigned if sqrt(1 / 0) succeeds...
@@ -11,7 +12,12 @@ except ZeroDivisionError as de:  # Catch an ZeroDivisionError.
     print(f"We got a division-by-zero error: {de}.", flush=True)
     try:  # Nesting try-except blocks is totally fine.
         sqrt_of_1_div_0 = sqrt(1 / 0)  # Let's try it again!
-    except ZeroDivisionError as de:  # Another ZeroDivisionError?
-        print(f"Yet another division-by-zero error: {de}.", flush=True)
+    except ZeroDivisionError:  # Another ZeroDivisionError?
+        print(f"Yet another division-by-zero error!", flush=True)
+        # We could also assign a value to sqrt_of_1_div_0 here, which
+        # would work, but we already chose the solution with an initial
+        # value before all the try-except blocks.
 
+print("Now we try to print the value of sqrt_of_1_div_0.", flush=True)
+print(sqrt_of_1_div_0)  # Works, because we gave an initial value.
 print("The program is now finished.")  # This time, we do get here.
