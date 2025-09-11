a217a016967a3a71629e80a776ea87ce2ad89873
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-11 10:03:50 +0800

    clearer example

--- exceptions/try_except_else.py
+++ exceptions/try_except_else.py
@@ -6,7 +6,7 @@ sqrt_of_1_div_0: float  # Declare this variable, but do not assign it.
 
 try:  # If this block raises DivisionByZero, we continue at `except`.
     sqrt_of_1_div_0 = sqrt(1 / 0)  # This is a DivisionByZero!
-except ZeroDivisionError as de:  # We catch and print the ZeroDivisionError.
+except ZeroDivisionError as de:  # Catch and print a ZeroDivisionError.
     print(f"We got a division-by-zero error: {de}.", flush=True)
 else:  # This code is not executed because an exception occurred.
     print(f"\u221A(1/0)\u2248{sqrt_of_1_div_0}")  # Never reached.
@@ -14,8 +14,8 @@ else:  # This code is not executed because an exception occurred.
 sqrt_3: float  # Declare this variable, but do not assign it.
 try:  # If this block raises ArithmeticError, we continue at `except`.
     sqrt_3 = sqrt(3.0)  # This will work just fine and raise no error.
-except ArithmeticError as ae:  # Or an ArithmeticError?
-    print(f"We got an arithmetic error: {ae}.", flush=True)  # Not done.
+except ZeroDivisionError:  # We catch and print the ZeroDivisionError.
+    print(f"We got a another division-by-zero error!", flush=True)
 else:  # This code is executed because no exception occurs.
     print(f"\u221A3\u2248{sqrt_3}")  # Always executed.
 
