ba821408aae789309d99edfed92ce9426fe3e563
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-14 13:46:38 +0800

    added comments

--- iteration/list_iteration.py
+++ iteration/list_iteration.py
@@ -1,21 +1,24 @@
-x = ["a", "b", "c"]
->>> for xi in x:
+x = ["a", "b", "c"]       # Create a list `x` of three strings.
+
+>>> for xi in x:    # We can loop over this list.
 ...     print(xi)
->>> for xi in x:
+
+>>> for xi in x:    # We can loop over this list as often as we want.
 ...     print(f"Hello letter {xi!r}!")
-from typing import Iterable, Iterator
-isinstance(x, Iterable)
-u = iter(x)
-isinstance(u, Iterator)
-type(u)
-v = iter(x)
-next(u)
-next(u)
-next(v)
-next(u)
-next(u)
-next(v)
-w = iter(x)
-next(w)
-next(v)
-next(v)
+
+from typing import Iterable, Iterator  # Import the two iteration types.
+isinstance(x, Iterable)  # Check if `x` can be iterated over.
+u = iter(x)              # Create an Iterator `u` over Iterable `x`.
+isinstance(u, Iterator)  # Check if `u` really is an `Iterator`.
+type(u)                  # The type of `u` is a subclass of `Iterator`.
+v = iter(x)              # Create an Iterator `v` over Iterable `x`.
+next(u)                  # Get first element in sequence `u`: `"a"`.
+next(u)                  # Get second element in sequence `u`: `"b"`.
+next(v)                  # Get first element in sequence `v`: `"a"`.
+next(u)                  # Get third element in sequence `u`: `"c"`.
+next(u)                  # `u` is exhausted, raises `StopIteration`.
+next(v)                  # Get second element in sequence `v`: `"b"`
+w = iter(x)              # Create an Iterator `w` over Iterable `x`.
+next(w)                  # Get first element in sequence `w`: `"a"`.
+next(v)                  # Get third element in sequence `v`: `"c"`.
+next(v)                  # `v` is exhausted, raises `StopIteration`.
