71909e03fb03e0487464e08aed834b5d075acbe7
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-15 08:42:11 +0800

    set iteration added

---
+++ iteration/set_iteration.py
@@ -0,0 +1,14 @@
+"""Iterating over a set: The resulting order is not clear!"""
+
+my_set: set[str] = {  # Create a set of the 26 Latin letters.
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+my_list: list[str] = []  # A list to receive the set elements.
+
+for element in my_set:   # Iterate over the set: The order is undefined.
+    my_list.append(element)
+
+# Merging all the letters in the order in which they were visited into a
+# single string and printing them. Each time we run this program, the
+# result is likely to be different.
+print("".join(my_list))
