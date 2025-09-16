887ac461276fd1ea25955edfe32bc54efc304473
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-16 12:00:41 +0800

    further improvement

--- iteration/simple_set_comprehension.py
+++ iteration/simple_set_comprehension.py
@@ -16,4 +16,8 @@ not_primes: set[int] = {k for k in range(2, 100)
                         for m in range(2, isqrt(k) + 1) if k % m == 0}
 # The set of numbers in 2..99 which are not in not_primes are primes.
 primes: set[int] = {n for n in range(2, 100) if n not in not_primes}
-print(f"prime numbers: {primes}")
+print(f"prime numbers 1: {primes}")
+
+# We could also use this method that creates a set from a range and uses
+# the set difference operator.
+print(f"prime numbers 2: {set(range(2, 100)).difference(not_primes)}")
