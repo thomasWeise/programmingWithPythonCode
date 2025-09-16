7c45859c790e8d5d4177f7b8df97b68cd5ac8045
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-16 11:37:29 +0800

    small improvement

--- iteration/simple_set_comprehension.py
+++ iteration/simple_set_comprehension.py
@@ -13,7 +13,7 @@ print(f"result of comprehension: {roots_2}")  # Print the result.
 
 # Compute the set of numbers in 2..99 which are not prime.
 not_primes: set[int] = {k for k in range(2, 100)
-                        for m in range(2, k) if k % m == 0}
+                        for m in range(2, isqrt(k) + 1) if k % m == 0}
 # The set of numbers in 2..99 which are not in not_primes are primes.
 primes: set[int] = {n for n in range(2, 100) if n not in not_primes}
 print(f"prime numbers: {primes}")
