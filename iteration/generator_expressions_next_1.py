a2fd7fa619ed77a6a93dc43afded583e298533ce
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-18 06:48:28 +0800

    fixed some issues.

--- iteration/generator_expressions_next_1.py
+++ iteration/generator_expressions_next_1.py
@@ -6,7 +6,7 @@ from typing import Generator, Iterator  # Import the types.
 gen: Generator[int, None, None] = (i ** 2 for i in range(1_000_000_000))
 
 # If `gen` was a tuple, `print(gen)` would print the tuple contents.
-print(gen)                # The contents of gen are <generator object...
+print(gen)  # However, the contents of gen are <generator object...>.
 print(f"{type(gen) = }")  # Type of `gen` is `generator`, not `tuple`...
 
 # Every `Generator` is also an `Iterator`, i.e., `Generator`s are
@@ -19,11 +19,10 @@ print(f"{isinstance(gen, Iterator) = }")
 print(f"{iter(gen) is gen = }")
 
 # Now let's manually iterate a bit over the generator expression `gen`.
-print(f"{next(gen) = }")  # Returns first element:  -5
-# 0
+print(f"{next(gen) = }")  # Returns first element:   0
 print(f"{next(gen) = }")  # Returns second element:  1
 print(f"{next(gen) = }")  # Returns third element:   4
 print(f"{next(gen) = }")  # Returns fourth element:  9
 print(f"{next(gen) = }")  # Returns fifth element:  16
-# We stop using the generator here, so the remaining 999999995 elements
-# are never generated.
+# We stop using the generator here, so the remaining 999'999'995
+# elements are never generated.
