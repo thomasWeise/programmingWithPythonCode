7522867aac7afe34e43f90ef1101f9b7fc3c53e3
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-18 06:35:40 +0800

    improved example

--- iteration/generator_expressions_next_1.py
+++ iteration/generator_expressions_next_1.py
@@ -1,12 +1,29 @@
 """The iteration behavior of generator expressions and `next`."""
 
-from typing import Generator  # Import the generator type hint.
+from typing import Generator, Iterator  # Import the types.
 
+# Create a generator expression of 1 billion square numbers.
 gen: Generator[int, None, None] = (i ** 2 for i in range(1_000_000_000))
-print(f"{type(gen) = }")  # Type is `generator`, not `tuple`
-print(f"{next(gen) = }")  # Returns first element = 0
-print(f"{next(gen) = }")  # Returns second element = 1
-print(f"{next(gen) = }")  # Returns third element = 4
-print(f"{next(gen) = }")  # Returns fourth element = 9
-# We stop using the generator here, so the remaining 999999996 elements
+
+# If `gen` was a tuple, `print(gen)` would print the tuple contents.
+print(gen)                # The contents of gen are <generator object...
+print(f"{type(gen) = }")  # Type of `gen` is `generator`, not `tuple`...
+
+# Every `Generator` is also an `Iterator`, i.e., `Generator`s are
+# special cases of `Iterator`s.
+print(f"{isinstance(gen, Iterator) = }")
+
+# Doing `iter(gen)` with `Generator` `gen` yields `gen` again.
+# This means that can iterate over a `Generator` only once, because we
+# cannot create independent `Iterators` of it (as we could for `list`s).
+print(f"{iter(gen) is gen = }")
+
+# Now let's manually iterate a bit over the generator expression `gen`.
+print(f"{next(gen) = }")  # Returns first element:  -5
+# 0
+print(f"{next(gen) = }")  # Returns second element:  1
+print(f"{next(gen) = }")  # Returns third element:   4
+print(f"{next(gen) = }")  # Returns fourth element:  9
+print(f"{next(gen) = }")  # Returns fifth element:  16
+# We stop using the generator here, so the remaining 999999995 elements
 # are never generated.
