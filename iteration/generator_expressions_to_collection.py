93b757a6f76611df1a8c6c7e33293cf77bc6518f
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-18 11:51:48 +0800

    added comments

--- iteration/generator_expressions_to_collection.py
+++ iteration/generator_expressions_to_collection.py
@@ -2,17 +2,22 @@
 
 csv_text: str = "22,56,33,67,43,33,12"
 
+# This could be more efficiently replaced by list comprehension.
 as_list: list[int] = list(int(i) for i in csv_text.split(","))
 print(f"a generator converted to a list: {as_list}.")
 
+# This cannot be replaced, because there is no tuple comprehension.
 as_tuple: tuple[int, ...] = tuple(int(i) for i in csv_text.split(","))
 print(f"a generator converted to a tuple: {as_tuple}.")
 
+# This could be more efficiently replaced by set comprehension.
 as_set: set[int] = set(int(i) for i in csv_text.split(","))
 print(f"a generator converted to a set: {as_set}.")
 
+# This cannot be replaced with a single line of code.
 as_sorted_list: list[int] = sorted(int(i) for i in csv_text.split(","))
 print(f"a generator converted to a sorted list: {as_sorted_list}.")
 
+# This could be more efficiently replaced by dictionary comprehension.
 as_dict: dict[str, int] = dict((i, int(i)) for i in csv_text.split(","))
 print(f"a generator of tuples converted to a dict: {as_dict}.")
