7480a620f673d55ed6125e276136840e720fd954
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-19 05:11:57 +0800

    simplification

--- iteration/filter_takewhile.py
+++ iteration/filter_takewhile.py
@@ -5,21 +5,15 @@ from math import isqrt  # the integer square root
 
 from prime_generator import primes  # prime number generator function
 
+# First, we want to create a list with all prime numbers less than 50.
+# This can be done using `takewhile` and a lambda expression.
 less_than_50: list[int] = list(takewhile(lambda z: z < 50, primes()))
 print(f"primes less than 50: {less_than_50}")
 
-
-def is_sqr_plus_1(x: int) -> bool:
-    """
-    Check whether `x` is of the form `y²+1`.
-
-    :param x: the number to check
-    :return: `True` if there is an integer `y` such that `x=y²+1`.
-    """
-    y: int = isqrt(x)
-    return x == (y ** 2) + 1
-
-
-sqrs_plus_1: list[int] = list(filter(is_sqr_plus_1, takewhile(
-    lambda z: z < 1000, primes())))
+# Now we want to find the prime numbers `x` < 1000 that have the form
+# `x = y² + 1` where `y` must be an integer number. For the latter
+# condition, we use a lambda expression and the `filter` function.
+sqrs_plus_1: tuple[int] = tuple(
+    filter(lambda x: x == isqrt(x) ** 2 + 1,  # check if x has form y²+1
+           takewhile(lambda z: z < 1000, primes())))  # Only z < 1000
 print(f"primes less than 1000 and of the form x²+1: {sqrs_plus_1}")
