e5c86f0dacc91198604f18ca35c9fdef88ce3ffd
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-18 13:24:35 +0800

    streamlined example

--- iteration/simple_generator_function_1.py
+++ iteration/simple_generator_function_1.py
@@ -9,11 +9,11 @@ def generator_123() -> Generator[int, None, None]:
     yield 2  # The second time next(...) is called, the result is 2.
     yield 3  # The third time next(...) is called, the result is 3.
 
-as_list: list[int] = list(generator_123())  # Create a list.
-print(f"{as_list = }")  # The list is [1, 2, 3].
+
+print(f"{list(generator_123()) = }")  # The list is [1, 2, 3].
 
 gen: Generator[int, None, None] = generator_123()  # Use directly.
-print(f"{next(gen) = }")  # First time next: get 1.
-print(f"{next(gen) = }")  # Second time next: get 2.
-print(f"{next(gen) = }", flush=True)  # Third time next: get 3.
+print(f"{next(gen) = }")              # First time next:  1
+print(f"{next(gen) = }")              # Second time next: 2
+print(f"{next(gen) = }", flush=True)  # Third time next:  3
 print(f"{next(gen) = }", flush=True)  # raises StopIteration
