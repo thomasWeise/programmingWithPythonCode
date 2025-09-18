f35e90141a82c04156e39a366b8c0f22a36a3f0b
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-18 11:16:32 +0800

    improved example a bit

--- iteration/generator_expressions_loops.py
+++ iteration/generator_expressions_loops.py
@@ -3,13 +3,15 @@
 from math import sin
 from typing import Generator
 
-# The generator expression produces tuples (n, sin(n)).
+# The generator expression produces tuples of the form `(n, sin(n))`.
 gen: Generator[tuple[int, float], None, None] = (
     (n, sin(n)) for n in range(1_000_000_000))
 
 # The for loop iterates over the generator expression and unpacks the
-# tuples into the variables o and p.
-for o, p in gen:
+# tuples into the variables `o` and `p`.
+for o, p in gen:  # `o` = original `n` and `p = sin(o) = sin(n)`
     if p < -0.99999:  # We look for an n with sin(n) < -0.99999.
         print(f"sin({o})={p}")  # We found it and print it.
         break  # And stop the iteration, not using the full range.
+else:  # This happens only if `break` was never invoked.
+    print("No value p < -0.99999 found.")
