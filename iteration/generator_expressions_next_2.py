718b347a6d0a2373a7a2b8327c8443b7f3e58e8e
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-18 08:07:08 +0800

    improved documentation

--- iteration/generator_expressions_next_2.py
+++ iteration/generator_expressions_next_2.py
@@ -1,11 +1,11 @@
-"""The iteration behavior of generator expressions and `next`."""
+"""The lazy iteration behavior of generator expressions and `next`."""
 
 from typing import Generator  # Import the generator type hint.
 
 
 def as_str(a: int) -> str:
     """
-    A simple function printing `a` and returning it as `str`.
+    A function with side effect printing `a` and returning it as `str`.
 
     :param a: the input number
     :return: the output
@@ -18,13 +18,15 @@ def as_str(a: int) -> str:
     return str(a)
 
 
-lst: list[str] = [as_str(j) for j in range(3)]
-print("list created")
-print(f"{next(iter(lst)) = }\n")
+# List comprehension immediately performs all `as_str` calls.
+lst: list[str] = [as_str(j) for j in range(3)]  # Prints 3 lines!
+print("list created")  # This text appears after the 3 lines.
+print(f"{next(iter(lst)) = }\n")  # This just prints '0'.
 
+# Generator invokes `as_str` only when required during iteration.
 gen: Generator[str, None, None] = (as_str(j) for j in range(3))
-print("generator created")
-print(f"{next(gen) = }")  # Prints input, then prints `"0"`
-print(f"{next(gen) = }")  # Prints input, then prints `"1"`
-print(f"{next(gen) = }", flush=True)  # Last print, `"2"`
+print("generator created")  # Nothing new is printed until now.
+print(f"{next(gen) = }")    # 2 prints: first in `as_str` and then `"0"`
+print(f"{next(gen) = }")    # 2 prints: first in `as_str` and then `"1"`
+print(f"{next(gen) = }", flush=True)  # Last 2 prints `as_str` + `"2"`
 print(f"{next(gen) = }", flush=True)  # Raises StopIteration
