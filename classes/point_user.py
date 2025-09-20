cf26d1dbe641a7c78191265bd6addb5186d1734f
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-20 11:31:15 +0800

    added one more comment

--- classes/point_user.py
+++ classes/point_user.py
@@ -1,6 +1,6 @@
 """Examples for using our class :class:`Point`."""
 
-from point import Point
+from point import Point            # Import our class from its module.
 
 p1: Point = Point(3, 5)            # Create a first instance of Point.
 print(f"{p1.x = }, {p1.y = }")     # p1.x = 3, p1.y = 5
