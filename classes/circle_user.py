8e67e262ec6575366d5afdb25cfcbcd1aa710c0a
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-25 07:07:35 +0800

    improved examle

--- classes/circle_user.py
+++ classes/circle_user.py
@@ -1,13 +1,22 @@
 """Examples for using our class :class:`Circle`."""
 
-from circle import Circle
-from point import Point
-from shape import Shape
+from circle import Circle  # Our new class `Circle`.
+from point import Point    # Our very first class ever: `Point`.
+from shape import Shape    # Our base class `Shape`.
 
-circ: Circle = Circle(Point(2, 3), 5)
-print(f"                  center: ({circ.center.x}, {circ.center.y})")
-print(f"                  radius: {circ.radius}")
-print(f"               perimeter: {circ.perimeter()}")
-print(f"                    area: {circ.area()}")
-print(f"isinstance(circ, Circle): {isinstance(circ, Circle)}")
-print(f" isinstance(circ, Shape): {isinstance(circ, Shape)}")
+circ: Circle = Circle(Point(2, 3), 5)  # Create a new circle instace.
+print(f"                    center: ({circ.center.x}, {circ.center.y})")
+print(f"                    radius: {circ.radius}")
+print(f"                 perimeter: {circ.perimeter()}")
+print(f"                      area: {circ.area()}")
+
+# Do some instance tests.
+print(f"  isinstance(circ, Circle): {isinstance(circ, Circle)}")
+print(f"   isinstance(circ, Shape): {isinstance(circ, Shape)}")
+print(f"  isinstance(circ, object): {isinstance(circ, object)}")
+
+# Explore the class hierarchy.
+print(f" issubclass(Circle, Shape): {issubclass(Circle, Shape)}")
+print(f" issubclass(Shape, Circle): {issubclass(Shape, Circle)}")
+print(f" issubclass(Shape, object): {issubclass(Shape, object)}")
+print(f"issubclass(Circle, object): {issubclass(Circle, object)}")
