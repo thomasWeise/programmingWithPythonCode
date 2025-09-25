2d284482b1866a820a3c3a9829536007d3a4f210
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-25 11:22:24 +0800

    small improvements

--- classes/shape_user.py
+++ classes/shape_user.py
@@ -5,9 +5,9 @@ from point import Point          # Our very first class ever: `Point`.
 from rectangle import Rectangle  # Our new class `Rectangle`.
 from triangle import Triangle    # Our new class `Triangle`.
 from shape import Shape          # Our base class `Shape`.
-from typing import Final         # Type hint for immutable variables.
 
-shapes: Final[list[Shape]] = [   # We create list of shapes.
+
+shapes: list[Shape] = [   # We create list of shapes.
     Circle(Point(2, 3), 5),
     Rectangle(Point(2, 3), Point(3, 5)),
     Triangle(Point(2, 3), Point(3, 5), Point(7, 4)),
