b016a0b4c0a6b1fc87c5d852a9314a1c5287091d
Thomas Weise <tweise@hfuu.edu.cn>
2025-09-26 10:34:24 +0800

    better comments

--- dunder/point_with_hash_user.py
+++ dunder/point_with_hash_user.py
@@ -2,8 +2,9 @@
 
 from point_with_hash import Point
 
-p1: Point = Point(3, 5)  # Create a first point.
-p2: Point = Point(7, 8)  # Create a second, different point.
+
+p1: Point = Point(3, 5)    # Create a first point.
+p2: Point = Point(7, 8)    # Create a second, different point.
 p3: Point = Point(3, 5.0)  # A third point, which equals the first.
 
 print(f"{(p1 == p2) = }")  # False, since p1 is really != p2
@@ -15,10 +16,12 @@ print(f"{p1 in points = }")  # True
 print(f"{p2 in points = }")  # True
 print(f"{p3 in points = }")  # True
 print(f"{Point(7.0, 8.0) in points = }")  # True: point is equal to p2
-print(f"{Point(3.1, 5) in points = }")  # False: point is not in the set
+print(f"{Point(3.1, 5) in points = }")    # False: point is not in set
 
 # A dictionary with points as keys.
 point_vals: dict[Point, str] = {p1: "A", p2: "B"}
-print(f"{point_vals = }")  #  {Point(3, 5): 'A', Point(7, 8): 'B'}
+print(f"{point_vals = }")      #  {Point(3, 5): 'A', Point(7, 8): 'B'}
 point_vals[Point(7, 9)] = "C"  # Put a new point/string-item in the dict
-print(f"{point_vals = }")  # Now there are three items.
+print(f"{point_vals = }")      # Now there are three items.
+point_vals[Point(3.0, 5.0)] = "D"  # Change value associated with p1.
+print(f"{point_vals = }")      # There are still three items.
