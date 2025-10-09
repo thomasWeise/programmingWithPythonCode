5b5ce961abaf7d51f84f41d60064e39e68d8470d
Thomas Weise <tweise@hfuu.edu.cn>
2025-10-10 05:11:29 +0800

    further improved comments

--- simple_datatypes/str_from.py
+++ simple_datatypes/str_from.py
@@ -1,13 +1,13 @@
 # Here are some functions can be used to convert strings to other types.
 
 int("1111")        # Convert decimal number 1111 to int.
-int("0x1111", 16)  # Convert hexadecimal number (base 16) 1111 to int.
-int("0b1111", 2)   # Convert binary number 1111 (base 2) to int.
+int("0x1111", 16)  # Convert hexadecimal number 1111 to int.
+int("0b1111", 2)   # Convert binary number 1111 to int.
 
 float("2.233e4")   # fractional number in scientific notation and
 float("0.1123")    # normal fractional number converted to float
-float("inf")       # "inf" is converted to the float value for infinity.
-float("nan")       # "nan" is converted to the float value not-a-number.
+float("inf")       # converted to the float value for infinity
+float("nan")       # converted to the float value not-a-number
 
 # ----> WARNING: The function `bool` works differently! <----
 # It performs a standard truth test, which returns `False` if its
@@ -15,9 +15,9 @@ float("nan")       # "nan" is converted to the float value not-a-number.
 # Otherwise, it returns True.
 # So it is actually True for the string "False".
 # "" is an empty string, but "False" is not an empty string....
-bool("True")       # convert to the bool value True
-bool("False")      # convert to the bool value True!!
-bool("")           # convert to the bool value False
+bool("True")       # "True" is converted to the bool value True.
+bool("False")      # "False" is converted to the bool value True!!!
+bool("")           # The empty string is converts to False.
 
 # We now have learned how to use these functions with string parameters.
 # However, they also work with parameters of other types.
