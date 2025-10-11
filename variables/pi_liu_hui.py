0631d78b44faba1f5838a117afa36ccabde03109
Thomas Weise <tweise@hfuu.edu.cn>
2025-10-12 05:29:29 +0800

    improved comments

--- variables/pi_liu_hui.py
+++ variables/pi_liu_hui.py
@@ -1,26 +1,29 @@
-from math import pi, sqrt
+from math import pi, sqrt   # We need sqrt. pi is for comparison.
 
+# We use f-strings with Unicode escapes to print the current result.
+# "\u03c0" is the Unicode escape for the Greek letter pi.
+# "\u2248" is the Unicode escape for the "approximately equal" sign.
 print(f"We use Liu Hui's Method to Approximate \u03c0\u2248{pi}.")
-e = 6  # the number of edges: We start with a hexagon, i.e., e=6.
+e = 6    # the number of edges: We start with a hexagon, i.e., e=6.
 s = 1.0  # the side length: Initially 1, meaning the radius is also 1.
 print(f"{e} edges, side length={s} give \u03c0\u2248{e * s / 2}.")
 
-e *= 2  # We double the number of edges...
+e *= 2   # We double the number of edges... ...now there are 12.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
 print(f"{e} edges, side length={s} give \u03c0\u2248{e * s / 2}.")
 
-e *= 2  # We double the number of edges.
+e *= 2  # We double the number of edges... ...now there are 24.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
 print(f"{e} edges, side length={s} give \u03c0\u2248{e * s / 2}.")
 
-e *= 2  # We double the number of edges.
+e *= 2  # We double the number of edges... ...now there are 48.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
 print(f"{e} edges, side length={s} give \u03c0\u2248{e * s / 2}.")
 
-e *= 2  # We double the number of edges.
+e *= 2  # We double the number of edges... ...now there are 96.
 s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
 print(f"{e} edges, side length={s} give \u03c0\u2248{e * s / 2}.")
 
-e *= 2  # We double the number of edges.
+e *= 2  # We double the number of edges... ...now there are 192.
 s = sqrt(2 - sqrt(4 - (s ** 2)))
 print(f"{e} edges, side length={s} give \u03c0\u2248{e * s / 2}.")
