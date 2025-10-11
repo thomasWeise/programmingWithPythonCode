ebcd28cc068edf76c95e2699aec05e5ffac9e483
Thomas Weise <tweise@hfuu.edu.cn>
2025-10-12 05:49:14 +0800

    slight layout improvement to better fit on slides

--- functions/use_my_math.py
+++ functions/use_my_math.py
@@ -10,6 +10,6 @@ print("We now use Liu Hui's Method to Approximate \u03c0.")
 e: int = 6  # the number of edges: We start with a hexagon, i.e., e=6.
 s: float = 1.0  # the side length: Initially 1, i.e., radius is also 1.
 for _ in range(6):
-    print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s / 2}.")
+    print(f"{e} edges, side length={s} give us \u03c0\u2248{e * s /2}.")
     e *= 2  # We double the number of edges...
-    s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length.
+    s = sqrt(2 - sqrt(4 - (s ** 2)))  # ...and recompute the side length
