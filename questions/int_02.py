574bb6cfec0d14125973393136f54c655c2b5ebd
Thomas Weise <tweise@hfuu.edu.cn>
2025-10-23 09:30:04 +0800

    added small example

---
+++ questions/int_02.py
@@ -0,0 +1,6 @@
+"""Fun with Arithmetics and Divisions."""
+
+print(7 * 3)    # Line 3: What is the output of this line?
+print(21 // 7)  # Line 4: What is the output of this line?
+print(21 / 7)   # Line 5: What is the output of this line?
+print(23 % 7)   # Line 6: What is the output of this line?
