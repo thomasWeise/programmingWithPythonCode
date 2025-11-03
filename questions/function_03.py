fce0d8756fd73fa127a7ccdbfceac679f3cfa256
Thomas Weise <tweise@hfuu.edu.cn>
2025-11-03 14:07:27 +0800

    added my_function example

---
+++ questions/function_03.py
@@ -0,0 +1,10 @@
+""" Guess the definition of `my_function`. """
+
+from function_03_module import my_function
+
+print(f"{my_function(1, 2, 3) = }")
+print(f"{my_function(a=7, b=5, c=6) = }")
+print(f"{my_function(9, 4) = }")
+print(f"{my_function(b=11, c=-3) = }")
+print(f"{my_function(a=-5, b=8) = }")
+print(f"{my_function(a=20, c=10) = }")
---
+++ questions/function_03_module.py
@@ -0,0 +1,12 @@
+""" An example of a function. """
+
+def my_function(a: int = 10, b: int = 6, c: int = 3) -> int:
+    """
+    Perform a computation.
+
+    :param a: the first number
+    :param b: the second number
+    :param c: the third number
+    :return: the output number
+    """
+    return a + b + c
