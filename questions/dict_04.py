8d022395f3b4dc012758a8219f363afd2a6afd33
Thomas Weise <tweise@hfuu.edu.cn>
2025-11-06 12:09:59 +0800

    added dict example

---
+++ questions/dict_04.py
@@ -0,0 +1,8 @@
+"""Dictionaries: What output is produced by this program?"""
+
+people: dict[str, tuple[str, int]] = {
+    "Bibbo": ("Chinese", 25), "Bebbo": ("French", 33),
+    "Bibba": ("Italian", 12), "Thomas": ("German", 44),
+    "Zhou": ("Chinese", 21), "Frank": ("English", 86),
+    "Wang": ("Chinese", 34), "Bodderich": ("Polish", 31)
+}
